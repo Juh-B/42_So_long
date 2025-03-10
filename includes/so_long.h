40f381dab11f4afa026c712f1c02f13cb866e2f3
Juliana Bonardi <106901927+Juh-B@users.noreply.github.com>
2025-03-10 16:19:25 -0300

    All done 2

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,7 +6,7 @@
 /*   By: jcosta-b <jcosta-b@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 14:41:28 by jcosta-b          #+#    #+#             */
-/*   Updated: 2025/02/26 18:35:06 by jcosta-b         ###   ########.fr       */
+/*   Updated: 2025/03/10 16:10:18 by jcosta-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,56 +44,65 @@
 # define RED "\033[0;91m"
 # define GREEN "\033[0;92m"
 # define YELLOW "\033[0;93m"
-# define WHITE "\033[0;97m"
+# define BOLD_G "\033[1;92m"
+# define BOLD_Y "\033[1;93m"
 
+# define WHITE "\033[0;97m"
 # define BLUE "\033[0;94m"
 # define GRAY "\033[0;90m"
 # define MAGENTA "\033[0;95m"
 # define CYAN "\033[0;96m"
 
+// Error mensage
+# define ERROR_P "The map must contain ONLY 1 PLAYER.\nHowever, it includes "
+# define ERROR_E "The map must contain ONLY 1 EXIT.\nHowever, it includes "
+
+
 typedef struct s_game {
-    size_t length;
-    size_t width;
-    size_t moves;
-    size_t collectible;
-    size_t exit;
-    size_t player;
-    int    player_col;
-    int    player_row;
-    char **map;
-    void *mlx_ptr;
-    void *mlx_win;
-    void  *img_floor;
-    void  *img_wall;
-    void  *img_collectible;
-    void  *img_player;
-    void  *img_exit;
-    int img_size;
-  } t_game;
+  size_t  length;
+  size_t  width;
+  size_t  moves;
+  size_t  collectible;
+  size_t  exit;
+  size_t  player;
+  int     player_col;
+  int     player_row;
+  char    **map;
+  void    *mlx_ptr;
+  void    *mlx_win;
+  void    *img_floor;
+  void    *img_wall;
+  void    *img_collectible;
+  void    *img_player;
+  void    *img_exit;
+  int     img_size;
+} t_game;
 
-  typedef struct s_verif_path {
-    char   **map;
-    size_t cols;
-    size_t rows;
-    int    found_exit;
-    int    found_collectibles;
-  } t_verif_path;
+typedef struct s_verif_path {
+  char   **map;
+  size_t cols;
+  size_t rows;
+  int    found_exit;
+  int    found_collectibles;
+} t_verif_path;
 
-  int	verif_map(char *map_file, t_game *game);
-  int	verif_map_struct(t_game *game, t_verif_path *verif_path);
-  int	verif_map_wall(t_game *game);
-  int	verif_map_path(t_game *game, t_verif_path *verif_path);
-  void	ft_free_map(t_verif_path *path);
-  void	fr_free_game(t_game *game);
+int	verif_map(char *map_file, t_game *game);
+int	verif_map_struct(t_game *game);
+int	verif_map_wall(t_game *game);
+int	verif_map_path(t_game *game);
 
-  void	init_game(t_game *game);
-  int	close_game(t_game *game);
-  int	handle_keypress(int keycode, t_game *game);
-  void	load_images(t_game *game);
-  void	draw_map(t_game *game);
+int	ft_error_file(int code);
+// void	ft_free_path(t_verif_path *path);
+void	fr_free_game(t_game *game);
 
-  int	ft_error_file(void);
-  void	fr_free_game(t_game *game);
+void	initialize_path(t_verif_path *verif_path);
+void	initialize_game(t_game *game);
+
+void	init_game(t_game *game);
+int	close_game(t_game *game);
+int	handle_keypress(int keycode, t_game *game);
+void	load_images(t_game *game);
+void	draw_map(t_game *game);
 
 #endif
 
